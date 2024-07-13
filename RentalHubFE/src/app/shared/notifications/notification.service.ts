2fbfe350b69a1bdd386593e58e964081a0d8ce43
nguyenkieuchauanh0908 <92566019+nguyenkieuchauanh0908@users.noreply.github.com>
2024-07-13 11:14:45 +0700

    [FIX] Fix marking notification as read API

--- RentalHubFE/src/app/shared/notifications/notification.service.ts
+++ RentalHubFE/src/app/shared/notifications/notification.service.ts
@@ -240,7 +240,7 @@ export class NotificationService {
   //Đánh dấu đã đọc toàn bộ
   markAsReadAll() {
     return this.http
-      .patch<resDataDTO>(
+      .get<resDataDTO>(
         environment.baseUrl + 'notification/read-all-notification',
         {}
       )
